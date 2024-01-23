735d11f4ff751ff82cd50fdcaadf3f895eaeb608
Derison <derison@derison.de>
2024-01-23 19:19:13 +0100

    Linter

--- Frontend/src/lib/components/Footer.svelte
+++ Frontend/src/lib/components/Footer.svelte
@@ -1,19 +1,11 @@
-<footer
-  class="p-4 footer bg-base-100 text-neutral-content fixed bottom-0"
->
+<footer class="p-4 footer bg-base-100 text-neutral-content fixed bottom-0">
   <div class="flex flex-col items-center p-4 w-full">
-    <p>
-      Sören Michaels © 2022 - All rights reserved
-    </p>
+    <p>Sören Michaels © 2022 - All rights reserved</p>
     <p class="text-gray-400">
-      <a
-        href="Impressum"
-        target="_blank"
-        class="text-xs hover:text-gray-300"
-      >
+      <a href="Impressum" target="_blank" class="text-xs hover:text-gray-300">
         Impressum
       </a>
-       |  
+      |
       <a
         href="Datenschutzerklaerung"
         target="_blank"
